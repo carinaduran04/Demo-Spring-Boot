90f2e0f9cdad1f678882b7a909d97ddacbc1e43e
carina <cariduran6420@gmail.com>
2025-08-27 17:22:10 -0400

    subida del proyecto

---
+++ demo-gestion/demo-gestion/src/test/java/com/demo/demo_gestion/DemoGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demo_gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
