66bb864e9f362a7abb562cf29c7362de4ec51206
carina <cariduran6420@gmail.com>
2025-09-19 16:56:09 -0400

    final mvp.2

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -33,7 +33,8 @@ export default function Login() {
         
         <div className="flex justify-center px-4">
           <Image
-            src="/logo15.png"   
+            src="/logo15.png"
+               
             alt="Logo"
             width={90}
             height={90}
