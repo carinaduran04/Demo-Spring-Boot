ce4e61c397c19dd9de8ed50ecffabc215ac5561f
carina <cariduran6420@gmail.com>
2025-09-19 16:28:57 -0400

    confijuracion

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+/*import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
@@ -6,6 +6,34 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         destination: "http://localhost:8080/api/:path*", // 👈 proxy al backend
+      },
+    ];
+  },
+};
+
+export default nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};npm 
+
+export default nextConfig;*/
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://72.60.26.63:8080/api/:path*", 
       },
     ];
   },
