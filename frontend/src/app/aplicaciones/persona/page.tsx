ed10b22041039618ad6e82587536f848ef39007b
carina <cariduran6420@gmail.com>
2025-09-19 16:34:48 -0400

    finalmvp

--- frontend/src/app/aplicaciones/persona/page.tsx
+++ frontend/src/app/aplicaciones/persona/page.tsx
@@ -1,11 +1,8 @@
-import PersonForm from "@/components/persona";
-/*
+
+
 export default function PersonaPage() {
   return (
-    <div className="mt-20 p-4">
-      <h1 className="text-xl font-bold mb-4">Crear Nueva Persona</h1>
-      <PersonForm />
-    </div>
+    <>
+    </>
   );
 }
-*/
