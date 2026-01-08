38423b7fda051f22f43d572e55a81b40dfbe0da8
carina <cariduran6420@gmail.com>
2026-01-08 09:18:20 -0500

    8090

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -33,7 +33,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*", 
+        destination: "http://localhost:8090/api/:path*", 
       },
     ];
   },
